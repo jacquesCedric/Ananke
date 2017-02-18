66f8e359c1d5cb7c92249dedd801fc8613675d5b
Jacob Gaffney <jacobgaffney@me.com>
2017-02-18 22:46:37 +1100

    Initial Commit

---
+++ megadl/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  megadl
+//
+//  Created by jacques on 18/02/2017.
+//  Copyright © 2017 Jacob Gaffney. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
